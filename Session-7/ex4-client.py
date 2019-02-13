9c5996a5b04cd6f502d69b94128b1bb8a979f3ed
estelasc <escarballo@hotmail.com>
2019-02-13 17:47:00 +0100

    Exercise 3 - my client Exercise 4 - client

--- Session-7/ex4-client.py
+++ Session-7/ex4-client.py
@@ -1,19 +1,20 @@
 # Client created to connect to my own server.
 import socket
 
-# Create a socker for communication with the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-print("Socket created")
 
 PORT = 1111
-IP = "192.168.1.34"
+IP = "212.128.253.69"
 
-# Connect to the server
-s.connect((IP, PORT))
 
 chat = True
 while chat:
+
+    # Create a socker for communication with the server
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect to the server
+    s.connect((IP, PORT))
+
     mess = input("Write your message: ")
     s.send(str.encode(mess))
 
@@ -25,4 +26,6 @@ while chat:
     if Exit == 'yes':
         chat = False
 
-s.close()
+    s.close()
+
+print("Chat has finished.")
