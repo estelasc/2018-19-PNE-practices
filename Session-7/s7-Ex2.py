fd5a4b16a9323d6c98fec3be64dbf2bbe1bdae7b
estelasc <escarballo@hotmail.com>
2019-02-13 10:54:05 +0100

    There is an error, but I will fix it soon.

--- Session-7/s7-Ex2.py
+++ Session-7/s7-Ex2.py
@@ -1,4 +1,5 @@
-# We are programming our first client
+# We are programming our first clientç
+# I know there is an error here that I must fix, but I don't know how to do it right know. I will fix it soon.
 
 import socket
 
@@ -7,25 +8,30 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 print("Socket created")
 
-PORT = 8080
-IP = "212.128.253.64"
+PORT = 9797
+IP = "192.168.1.34"
 
 # Connect to the server
+s.connect((IP, PORT))
+
+
 Chat = True
 while Chat:
-    s.connect((IP, PORT))
 
-    s.send(str.encode(input("Enter the message you want to send:")))
+    # Why when I send the third message to the server, it stop working (BrokenPipeError)???
+    mess = input("Enter the message you want to send to the teacher's server: ")
+    s.send(str.encode(mess))
 
     msg = s.recv(2048).decode("utf-8")
     print("MESSAGE FROM THE SERVER: ")
     print(msg)
 
-    s.close()
-
     Exit = input("Write 'yes' if you want to exit.")
-    Exit.lower()
+    Exit = Exit.lower()
+    Exit = Exit.replace(" ", "")
     if Exit == "yes":
         Chat = False
 
-print("All is ok")
+
+s.close()
+print("The chat has finished.")
