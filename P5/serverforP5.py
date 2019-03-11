700c99f61f2224441da8b33a1018ad0982d24f50
estelasc <escarballo@hotmail.com>
2019-03-11 22:26:07 +0100

    Server for practice 5

---
+++ P5/serverforP5.py
@@ -0,0 +1,82 @@
+# Server for practice 5. Same as server for practice 4 but using http instead of html.
+import http.server
+import socketserver
+import termcolor
+
+# Define the Server's port
+PORT = 2000
+
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inheritates all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+
+        # Print the request line
+        termcolor.cprint(self.requestline, 'red')
+
+        # IN this simple server version:
+        # We are NOT processing the client's request
+        # It is a happy server: It always returns a message saying
+        # that everything is ok
+
+        # Message to send back to the clinet
+        if self.path == '/' or self.path == '/index' or self.path == '/index.html':
+            file = open('index.html', 'r')
+            contents = file.read()
+            file.close()
+        elif self.path == '/blue' or self.path == '/blue.html':
+            file = open('blue.html', 'r')
+            contents = file.read()
+            file.close()
+        elif self.path == '/pink' or self.path == '/pink.html':
+            file = open('pink.html', 'r')
+            contents = file.read()
+            file.close()
+        else:
+            print(self.path)
+            file = open('error.html', 'r')
+            contents = file.read()
+            file.close()
+
+        # Generating the response message
+        self.send_response(200)  # -- Status line: OK!
+
+        # Define the content-type header:
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(contents)))
+
+        # The header is finished
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(str.encode(contents))
+
+        return
+
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stoped by the user")
+        httpd.server_close()
+
+print("")
+print("Server Stopped")
