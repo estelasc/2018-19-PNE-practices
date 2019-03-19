3061fa2c12f87d59bc92816ae92eeefaf699ed3f
estelasc <escarballo@hotmail.com>
2019-03-19 16:52:53 +0100

    Some work for session 17

---
+++ Session-17/test_json.py
@@ -0,0 +1,21 @@
+import json
+import termcolor
+
+f = open("person.json", 'r')
+
+person = json.load(f)
+
+print()
+
+termcolor.cprint("Name: ", 'green', end='')
+print(person['Firstname'], person['Lastname'])
+termcolor.cprint("Age: ", 'green', end='')
+print(person['age'])
+
+for i, num in enumerate(person["phoneNumber"]):
+    print("Phone {}: ".format(i))
+
+    termcolor.cprint("      Type: ", 'red', end='')
+    print(num["type"])
+    termcolor.cprint("      Number: ", 'red', end='')
+    print(num['number'])
